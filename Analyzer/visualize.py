cbc9a91c10d29431800094afaab4d48ffd3a4146
tokikuch <k.toshihito@yahoo.de>
2016-07-18 12:48:42 -0700

    Single-threaded time series DFT

--- Analyzer/visualize.py
+++ Analyzer/visualize.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import time
 import struct
 from neurosky import stream as ns
 import numpy as np
@@ -7,9 +8,10 @@ import cv2
 
 def loadSample(findex):
     samples = [
+        '2016-07-18-07-09-34-RawPackageRecording.txt',
         '2016-07-17-16-16-30-RawPackageRecording.txt',
         '2016-07-17-12-29-04-RawPackageRecording.txt',
-        '2016-07-16-15-08-44-RawPackageRecording.txt',
+        '2016-07-16-15-08-44-RawPackageRecording.txt', # short
         '2016-07-15-18-17-19-RawPackageRecording.txt',
         '2016-07-15-07-12-33-RawPackageRecording.txt',
         '2016-07-14-07-46-46-RawPackageRecording.txt',
@@ -52,25 +54,44 @@ def loadSample(findex):
 def printValues(array, log_threshold):
     it = np.nditer(array, flags=['multi_index'])
     while not it.finished:
-        if it[0] > .0 and np.log(it[0]) > log_threshold:
+        x = np.abs(it[0])
+        if x > .0 and np.log(x) > log_threshold:
             print "[%s] = %.4e" % (it.multi_index, it[0])
         it.iternext()
 
-def saveFrequencyDomain(array, imgHeight, imgName):
-    width = len(array)
-    im_complex = np.zeros((1, width, 2), dtype=np.float64)
-    im_complex[:, :, 0] = array
+def createStackedMatrix(array, width):
+    height = len(array) - width + 1
+    longer = max(width, height)
+    matrix = np.zeros((height, width, 2), dtype=np.float64)
+    it = np.nditer(array, ['c_index'], ['readonly'])
+    while not it.finished:
+        x = it.index if it.index < width else width - 1
+        y = it.index - x
+        while x >= 0 and y < height:
+            matrix[y, x, 0] = it[0]
+            x -= 1
+            y += 1
+        it.iternext()
+    return matrix
+
+def saveFrequencyDomain(array, timeWindow, imgName):
+    im_complex = createStackedMatrix(array, timeWindow)
     im_complex = cv2.dft(im_complex, dst=im_complex, flags=cv2.DFT_ROWS)
     im_re, im_im = cv2.split(im_complex)
     im_mag = cv2.magnitude(im_re, im_im)
     # printValues(im_mag, -10)
-    im_mag = im_mag[0][0:len(im_mag[0])/2]
-    im_gray = np.zeros(np.shape(im_mag))
-    im_gray = cv2.normalize(im_mag, im_gray, 0, 255, cv2.NORM_MINMAX)
-    im = np.tile(im_gray, (imgHeight, 1))
+    im_gray = im_mag[:,:len(im_mag[0])/2]
+    im_gray = cv2.normalize(im_gray, im_gray, 0, 255, cv2.NORM_MINMAX)
+    im = np.transpose(im_gray)
     cv2.imwrite(imgName, im)
 
 if __name__ == '__main__':
-    idx = int(sys.argv[1]) if len(sys.argv) > 1 else 2
+    idx = int(sys.argv[1]) if len(sys.argv) > 1 else 3
+    t = time.time()
+    print "> Start loading data.."
     x = loadSample(idx)
-    saveFrequencyDomain(x[0:1024], 100, 'dft.png')
+    print "Done.  Elapsed(sec.) = %.3f" % (time.time() - t)
+    t = time.time()
+    print "> Start DFT.."
+    saveFrequencyDomain(x, 2048, 'dft.png')
+    print "Done.  Elapsed(sec.) = %.3f" % (time.time() - t)
